77b1a66e213e5709980db1cbe3ba128d9cef5b5a
Mender24 <Nikkvinsi@Gmail.com>
2025-11-03 12:48:08 +0300

    BuildSet

--- Assets/Skripts/Controllers/FishingController.cs
+++ Assets/Skripts/Controllers/FishingController.cs
@@ -1,10 +1,7 @@
-using Microsoft.Unity.VisualStudio.Editor;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 
 public class FishingController : MonoBehaviour
