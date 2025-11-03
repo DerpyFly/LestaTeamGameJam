b77dbbab08788c1ae3dbc7704db42b83afa0e184
NasusMain <107574485+NasysMeiner@users.noreply.github.com>
2025-11-03 05:23:35 +0300

    fix

---
+++ Assets/Skripts/Torhes/Torche.cs
@@ -0,0 +1,27 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class Torche : MonoBehaviour, IInteractable
+{
+    [SerializeField] private GameObject _pointPositionPearl;
+
+    private Torches _torches;
+
+    private SphereCollider _sphereCollider;
+
+    private void Awake()
+    {
+        _torches = transform.GetComponentInParent<Torches>();
+    }
+
+    public void EndInteractable()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Interact(PlayerAll playerAll)
+    {
+        throw new System.NotImplementedException();
+    }
+}
